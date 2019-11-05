32f8167c3e259dd1c1fc27d58bc59327083b4797
Frankde-mua <38346112+Frankde-mua@users.noreply.github.com>
2019-11-05 09:30:03 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>{{ config('Health Focus Video Store')}}</title>
+  <title>{{ 'Health Focus Video Store'}}</title>
 
   <!-- Scripts -->
   <script src="{{ asset('js/app.js') }}" defer></script>
